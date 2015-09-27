9f4afc785d57d3f41d8668db15deb420d7b33b1f
Oleh Kuchuk <kuchuklehjs@gmail.com>
2015-09-28 00:30:06 +0300

    add once method

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -17,6 +17,7 @@ EventEmitter.prototype.on = function(evt, handler) {
 	this.config.root.addEventListener(evt, function (e) {
 		handler(e);
 	}, this.config.useCapture);
+
 	var evts = this.evtHash;
 	if (!evts.hasOwnProperty(evt)) {
 		evts[evt] = {
@@ -30,6 +31,30 @@ EventEmitter.prototype.on = function(evt, handler) {
 	}
 
 	return this;
+};
+
+EventEmitter.prototype.once = function(evt, handler) {
+	var customEvt = new Event(evt);
+	var self = this;
+	var config = self.config;
+	var evtHash = self.evtHash;
+
+	var listener = function (e) {
+		handler(e);
+		self.removeListener(evt, listener);
+		delete self.evtHash[evt];
+	};
+
+	config.root.addEventListener(evt, listener, config.useCapture);
+
+	if (!evtHash.hasOwnProperty(evt)) {
+		evtHash[evt] = {
+			evt: customEvt,
+			handlers: [handler]
+		};
+	} else {
+		evtHash[evt].handlers.push(handler);
+	}
 };
 
 EventEmitter.prototype.emit = function (evt) {
@@ -49,22 +74,26 @@ EventEmitter.prototype.setMaxListeners = function (num) {
 	if (typeof num === 'number' && num) {
 		this.config.maxListeners = num;
 	}
-}
+};
 
 EventEmitter.prototype.removeListener = function (evt, handler) {
-	this.config.root.removeEventListener(evt, handler, this.config.useCapture);
+	if (this.evtHash.hasOwnProperty(evt)) {
+		this.config.root.removeEventListener(evt, handler, this.config.useCapture);
+	}
 };
 
 EventEmitter.prototype.removeAllListeners = function (evt) {
-	var allHandlers = this.evtHash[evt].handlers;
-	var len = allHandlers.length;
+	var evtObj = this.evtHash[evt];
+	var len = evtObj.handlers.length;
 	var root = this.root;
 	var capture = this.config.useCapture, i;
 
 	for (	i = 0; i < allHandlers.length; i++ ) {
-		root.removeEventListener(evt, allHandlers[i], capture);
+		root.removeEventListener(evt, evtObj.handlers[i], capture);  //use already defined method
 	}
-}
+
+	delete evtObj;
+};
 
 EventEmitter.prototype.listenerCount = function (evt) {
 	if (this.evtHash.hasOwnProperty(evt)) {
@@ -72,17 +101,19 @@ EventEmitter.prototype.listenerCount = function (evt) {
 	} else {
 		throw new Error('There is no such event');
 	}
-}
+};
 
 EventEmitter.prototype.listeners = function (evt) {
 	var allEvents = this.evtHash;
 	if (allEvents.hasOwnProperty(evt)) {
 		return allEvents[evt].handlers;
 	}
-}
+};
 
 if (typeof module !== 'undefined' && module.exports) {
 	module.exports = EventEmitter;
 } else {
 	window.EventEmitter = EventEmitter;
 }
+
+window.EventEmitter = EventEmitter;
