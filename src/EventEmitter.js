4f21a4dd672b9bb77f11ce8ab88d14c9684f9043
Oleh Kuchuk <kuchuklehjs@gmail.com>
2015-10-02 19:29:45 +0300

    improve

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -2,30 +2,41 @@
 
 var helpers = require('./EventEmitter.helpers');
 
+/**
+ * [EventEmitter description]
+ * @param {[type]} config [description]
+ */
 var EventEmitter = function (config) {
-	config = config || {};
-	if (!helpers.checkObj(config)) throw new TypeError('Should be an object');
-	this.evtHash= {};
-	this.config = {
-		maxListeners: config.maxListeners || 10,
-		handlersDelay: config.handlersDelay || 0
-	};
+	if (this instanceof EventEmitter) {
+		config = config || {};
+		if (!helpers.checkObj(config)) throw new TypeError('Should be an object');
+		this.evtHash= {};
+		this.config = {
+			maxListeners: config.maxListeners || 10,
+			handlersDelay: config.handlersDelay || 0
+		};
+	} else {
+		return new EventEmitter(config);
+	}
 };
 
+
+/**
+ *
+ * @param  {[type]} evt     [description]
+ * @param  {[type]} handler [description]
+ * @return {[type]}         [description]
+ */
 EventEmitter.prototype.on = function(evt, handler) {
-	var customEvt = new Event(evt);
 	var self = this;
 	var evts = self.evtHash;
 	var config = self.config;
 
 	if (!evts.hasOwnProperty(evt)) {
-		evts[evt] = {
-			evt: customEvt,
-			handlers: [handler]
-		};
+		evts[evt] = [handler];
 	} else {
 		if (evts[evt].handlers.length < config.maxListeners) {
-			evts[evt].handlers.push(handler);
+			evts[evt].push(handler);
 		} else {
 			throw new Error('Can not add more than ' + config.maxListeners + ' handlers');
 		}
@@ -36,16 +47,20 @@ EventEmitter.prototype.on = function(evt, handler) {
 
 EventEmitter.prototype.addListener = EventEmitter.prototype.on;
 
+
+/**
+ * [once description]
+ * @param  {[type]} evt     [description]
+ * @param  {[type]} handler [description]
+ * @return {[type]}         [description]
+ */
 EventEmitter.prototype.once = function(evt, handler) {
 	var customEvt = new Event(evt);
 	var self = this;
 	var config = self.config;
 	var evtHash = self.evtHash;
 
-	var listener = function () {
-		handler();
-	}
-	if (!evtHash.hasOwnProperty(evt)) {
+	if (!helpers.hasProperty(evtHash, evt)) {
 		evtHash[evt] = {
 			evt: customEvt,
 			handlers: [handler]
@@ -55,31 +70,66 @@ EventEmitter.prototype.once = function(evt, handler) {
 	}
 };
 
-EventEmitter.prototype.emit = function (evt) {
-	var evts = this.evtHash;
-	if (evts.hasOwnProperty(evt)) {
 
+/**
+ * [emit description]
+ * @param  {[type]} evt [description]
+ * @return {[type]}     [description]
+ */
+EventEmitter.prototype.emit = function (evt) {
+	var args = ([]).slice.call(arguments, 1);
+	var evts = this.evtHash;
+	if (helpers.hasProperty(evts, evt)) {
+		evts[evt].forEach(function (handler) {
+			handler(args);
+		});
 	} else {
 		throw new Error('There is now such event handler');
 	}
 };
 
+
+/**
+ * [getMaxListeners description]
+ * @return {[type]} [description]
+ */
 EventEmitter.prototype.getMaxListeners = function () {
 	return this.config.maxListeners;
 };
 
+
+/**
+ * [setMaxListeners description]
+ * @param {[type]} num [description]
+ */
 EventEmitter.prototype.setMaxListeners = function (num) {
 	if (typeof num === 'number' && num) {
 		this.config.maxListeners = num;
 	}
 };
 
+
+/**
+ * [removeListener description]
+ * @param  {[type]} evt     [description]
+ * @param  {[type]} handler [description]
+ * @return {[type]}         [description]
+ */
 EventEmitter.prototype.removeListener = function (evt, handler) {
-	if (this.evtHash.hasOwnProperty(evt)) {
-		this.config.root.removeEventListener(evt, handler, this.config.useCapture);
+	var allEvts = this.evtHash;
+	if (helpers.hasProperty(allEvts, evt)) {
+		var index = allEvts[evt].indexOf(handler);
+		if (index < 0) return;
+		return allEvts[evt].splice(index, 1);
 	}
 };
 
+
+/**
+ * [removeAllListeners description]
+ * @param  {[type]} evt [description]
+ * @return {[type]}     [description]
+ */
 EventEmitter.prototype.removeAllListeners = function (evt) {
 	var evtObj = this.evtHash[evt];
 	var len = evtObj.handlers.length;
@@ -93,18 +143,31 @@ EventEmitter.prototype.removeAllListeners = function (evt) {
 	delete this.evtHash[evt];
 };
 
+
+/**
+ * [listenerCount description]
+ * @param  {[type]} evt [description]
+ * @return {[type]}     [description]
+ */
 EventEmitter.prototype.listenerCount = function (evt) {
-	if (this.evtHash.hasOwnProperty(evt)) {
-		return this.evtHash[evt].handlers.length;
+	var allEvts = this.evtHash;
+	if (helpers.hasProperty(allEvts, evt)) {
+		return allEvts[evt].length;
 	} else {
 		throw new Error('There is no such event');
 	}
 };
 
+
+/**
+ * [listeners description]
+ * @param  {[type]} evt [description]
+ * @return {[type]}     [description]
+ */
 EventEmitter.prototype.listeners = function (evt) {
-	var allEvents = this.evtHash;
-	if (allEvents.hasOwnProperty(evt)) {
-		return allEvents[evt].handlers;
+	var evts = this.evtHash;
+	if (helpers.hasProperty(evts, evt)) {
+		return allEvents[evt];
 	}
 };
 
--- test/EventEmitter.js
+++ test/EventEmitter.js
@@ -2,7 +2,34 @@ var assert = require('chai').assert;
 var EventEmitter = require('../src/EventEmitter');
 
 describe('EventEmitter test case', function () {
+	var em = new EventEmitter();
+	var em2 = EventEmitter();
+
 	it('should create correct EventEmmiter instance', function (done) {
+		assert.strictEqual(em instanceof EventEmitter, true, 'Should be an instanceof EventEmitter');
+		assert.strictEqual(em2 instanceof EventEmitter, true, 'Also should be instance even without new');
+		done();
+	});
+
+	it('Should return correct max listeners length', function (done) {
+		var em = new EventEmitter();
+		assert.strictEqual(em.getMaxListeners(), 10, 'default value fo event emitter should be correct');
+		em.setMaxListeners(5);
+		assert.strictEqual(em.getMaxListeners(), 5, 'Should allow user to change def value');
+		em.setMaxListeners('bad value');
+		assert.strictEqual(em.getMaxListeners(), 5, 'Should not work');
+		done();
+	});
+
+	it('Should add events', function (done) {
+
+		done();
+	});
+
+	it('Should return correct length of listeners for event', function (done) {
+		assert.strictEqual('', function () {
+
+		});
 		done();
 	});
 });
